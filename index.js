a4ab00e38539bd9f4bb3be850e54a35bf26bf82b
Vaibhav Chavan <vaibhav_chavan@idealake.com>
2024-05-08 00:15:53 +0530

    Returning Values From Async Functions

--- index.js
+++ index.js
@@ -33,10 +33,34 @@ const getDogPic = async () => {
     console.log('Random dog image saved to file');
   } catch (err) {
     console.log(err);
+    throw err;
   }
+  return '2: Ready';
 };
 
-getDogPic();
+(async () => {
+  try {
+    console.log('1: Will get dog pics');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('3: Done getting dog pics');
+  } catch (err) {
+    console.log('ERROR');
+  }
+})();
+
+/*
+console.log('1: Will get dog pics');
+getDogPic()
+  .then((x) => {
+    console.log(x);
+    console.log('3: Done getting dog pics');
+  })
+  .catch((err) => {
+    console.log('ERROR');
+  });
+
+*/
 
 /*
 readFilePro(`${__dirname}/dog.txt`)
